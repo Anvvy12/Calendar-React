cc9932bc33ff65722e41617bd354a2753842dd50
Bogdan <youbody7@gmail.com>
2022-09-22 20:47:56 +0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,51 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/header/Header.jsx";
 import Calendar from "./components/calendar/Calendar.jsx";
+import { months } from "./utils/dateUtils";
 
 import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
 
 import "./common.scss";
 
-class App extends Component {
-  today = new Date();
-  week = 7;
-  state = {
-    weekStartDate: new Date(this.today.setDate(this.today.getDate())),
-  };
-  ShowWeekAfterActual = () => {
-    this.setState({
-      weekStartDate: new Date(
-        this.today.setDate(this.today.getDate() + this.week)
-      ),
-    });
-  };
-  ShowWeekBeforeActual = () => {
-    this.setState({
-      weekStartDate: new Date(
-        this.today.setDate(this.today.getDate() - this.week)
-      ),
-    });
+const App = () => {
+  const today = new Date();
+
+  const [actualDay, setNewWeek] = useState(
+    new Date(today.setDate(today.getDate()))
+  );
+
+  const a = () => {
+    const firsDayOfWeek = months[getWeekStartDate(actualDay).getMonth()];
+    const lastDayOfWeek =
+      months[generateWeekRange(getWeekStartDate(actualDay))[6].getMonth()];
+    let res = firsDayOfWeek;
+    if (firsDayOfWeek !== lastDayOfWeek) {
+      res += " - " + lastDayOfWeek;
+    }
+    return res;
   };
 
-  render() {
-    const { weekStartDate } = this.state;
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+  const ShowWeekAfterActual = () =>
+    setNewWeek(new Date(actualDay.setDate(actualDay.getDate() + 7)));
 
-    return (
-      <>
-        <Header
-          afterActualWeek={this.ShowWeekAfterActual}
-          beforeActualWeek={this.ShowWeekBeforeActual}
-        />
-        <Calendar weekDates={weekDates} />
-      </>
-    );
-  }
-}
+  const ShowWeekBeforeActual = () =>
+    setNewWeek(new Date(actualDay.setDate(actualDay.getDate() - 7)));
+
+  const showActualWeek = () => {
+    setNewWeek(new Date(today.setDate(today.getDate())));
+  };
+
+  return (
+    <>
+      <Header
+        afterActualWeek={ShowWeekAfterActual}
+        beforeActualWeek={ShowWeekBeforeActual}
+        showActualWeek={showActualWeek}
+        a={a}
+      />
+      <Calendar weekDates={generateWeekRange(getWeekStartDate(actualDay))} />
+    </>
+  );
+};
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-
 import "./header.scss";
 
-const Header = ({ afterActualWeek, beforeActualWeek }) => {
+const Header = ({ afterActualWeek, beforeActualWeek, showActualWeek, a }) => {
   return (
     <header className="header">
       <button className="button create-event-btn">
         <i className="fas fa-plus create-event-btn__icon"></i>Create
       </button>
       <div className="navigation">
-        <button className="navigation__today-btn button">Today</button>
+        <button
+          className="navigation__today-btn button"
+          onClick={showActualWeek}
+        >
+          Today
+        </button>
         <button
           className="icon-button navigation__nav-icon"
           onClick={beforeActualWeek}
@@ -22,7 +26,7 @@ const Header = ({ afterActualWeek, beforeActualWeek }) => {
         >
           <i className="fas fa-chevron-right"></i>
         </button>
-        <span className="navigation__displayed-month"></span>
+        <span className="navigation__displayed-month">{a()}</span>
       </div>
     </header>
   );
