af2bdae175ae6d0f0aedbee27f0ef808703736dc
Bogdan <youbody7@gmail.com>
2022-10-25 12:28:03 +0300

    s

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -12,17 +12,22 @@ const Event = ({ title, time, id, handleOnDelete, refreshPage }) => {
   const [isVisible, setVisible] = useState(false);
 
   const showDeleteBtn = () => {
-    setVisible(true);
-    const stateBtn = isVisible ? "open" : "hiden";
-    return stateBtn;
+    setVisible(!isVisible);
   };
+
+  const buttonDelete = isVisible ? "open" : "hidden";
 
   return (
     <div className="event" onClick={showDeleteBtn}>
       <div className="event__title">{title}</div>
       <div className="event__time">{time}</div>
-      <div className="hiden" onClick={handleDelete}>
-        <button className="delete-form__btn">Delete</button>
+      <div className={`${buttonDelete} div-delete`}>
+        <button
+          className="delete-event-btn btn"
+          onClick={() => handleDelete(id)}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
